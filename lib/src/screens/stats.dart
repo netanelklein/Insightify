69763170957f956c171f5dc1a9cb9c86c6f54e01
netanelklein <netanel@netanelk.com>
2023-08-06 13:15:23 +0300

    Chsnge scroll to slivers

--- lib/src/screens/stats.dart
+++ lib/src/screens/stats.dart
@@ -10,34 +10,34 @@ class Stats extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: FutureBuilder(
-        future: Future.wait([totalPlayedTime, mostStreamedDay]),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return (Column(
-              children: [
-                Card(
-                  child: ListTile(
-                    title: const Text('Total Time Played:'),
-                    subtitle: Center(
-                        child: Text(msToTimeString(
-                            snapshot.data![0][0]['total_ms_played']))),
-                  ),
+    return FutureBuilder(
+      future: Future.wait([totalPlayedTime, mostStreamedDay]),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return SliverFillRemaining(
+              child: Column(
+            children: [
+              Card(
+                child: ListTile(
+                  title: const Text('Total Time Played:'),
+                  subtitle: Center(
+                      child: Text(msToTimeString(
+                          snapshot.data![0][0]['total_ms_played']))),
                 ),
-                Card(
-                    child: ListTile(
-                  title: const Text('Most Streamed Day:'),
-                  subtitle: Text(
-                      'The most streamed day was ${snapshot.data![1][0]['day']}.\nYou played ${msToTimeString(snapshot.data![1][0]['total_ms_played'])} on this day.'),
-                ))
-              ],
-            ));
-          } else {
-            return const Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
+              ),
+              Card(
+                  child: ListTile(
+                title: const Text('Most Streamed Day:'),
+                subtitle: Text(
+                    'The most streamed day was ${snapshot.data![1][0]['day']}.\nYou played ${msToTimeString(snapshot.data![1][0]['total_ms_played'])} on this day.'),
+              ))
+            ],
+          ));
+        } else {
+          return SliverFillRemaining(
+              child: Center(child: CircularProgressIndicator()));
+        }
+      },
     );
   }
 }
