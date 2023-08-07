03aff3b755bfe72e6ff91ee266cef4fd7629af55
netanelklein <netanel@netanelk.com>
2023-08-07 16:50:46 +0300

    Created 'settings' screen

---
+++ lib/src/screens/settings.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import '../widgets/settings/min_count.dart';
+import '../widgets/settings/order_by.dart';
+import '../widgets/settings/clear_data.dart';
+
+class SettingsScreen extends StatefulWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings'),
+        leading: IconButton(
+            onPressed: () => Navigator.pop(context),
+            icon: const Icon(Icons.arrow_back)),
+        elevation: 2,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            // const MinCount(),
+            // const Divider(),
+            // const SetOrderBy(),
+            // const Divider(),
+            const ClearData(),
+          ],
+        ),
+      ),
+    );
+  }
+}
