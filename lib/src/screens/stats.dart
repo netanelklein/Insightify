50c4bb25d02202275cf55fe13b001a000019d7c7
netanelklein <netanel@netanelk.com>
2023-08-23 13:52:44 +0300

    Added time of the day stats and refactored

--- lib/src/screens/stats.dart
+++ lib/src/screens/stats.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
-import '../utils/functions.dart';
+import '../widgets/stats/average_listening_day.dart';
+import '../widgets/stats/favourite_time_of_day.dart';
+import '../widgets/stats/most_streamed_day.dart';
+import '../widgets/stats/stats_time_range.dart';
+import '../widgets/stats/total_play_time.dart';
 import '../utils/database_helper.dart';
 import '../../app_state.dart';
 
@@ -20,66 +23,30 @@ class _StatsState extends State<Stats> {
     final totalPlayedTime = DatabaseHelper().getTotalTimePlayed(timeRange);
     final mostStreamedDay = DatabaseHelper().getMostPlayedDay(timeRange);
     final averageListeningDay = DatabaseHelper().getAverageDay(timeRange);
+    final timeOfDay = DatabaseHelper().getTimeOfDay(timeRange);
+
     return FutureBuilder(
-      future:
-          Future.wait([totalPlayedTime, mostStreamedDay, averageListeningDay]),
+      future: Future.wait(
+          [totalPlayedTime, mostStreamedDay, averageListeningDay, timeOfDay]),
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           if (snapshot.data![0][0]['total_ms_played'] == null) {
             return const Center(child: Text('No data found'));
           }
           return Consumer<AppState>(builder: (context, appState, _) {
-            return Column(
+            return ListView(
               children: [
                 !appState.isMaxRange
-                    ? Card(
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const Text('Time Frame:',
-                                    style: TextStyle(fontSize: 20)),
-                                Text(
-                                    'The shown statistics are from ${DateFormat.yMd().format(appState.timeRange.start)} to ${DateFormat.yMd().format(appState.timeRange.end)}.'),
-                                Align(
-                                  alignment: Alignment.centerRight,
-                                  child: FilledButton(
-                                      onPressed: () {
-                                        DatabaseHelper()
-                                            .getMaxDateRange()
-                                            .then((value) {
-                                          appState.setTimeRange = value;
-                                          appState.setIsMaxRange = true;
-                                        });
-                                      },
-                                      child: const Text(
-                                          'Show All Time Statistics')),
-                                )
-                              ]),
-                        ),
-                      )
+                    ? StatsTimeRange(appState: appState)
                     : Container(),
-                Card(
-                  child: ListTile(
-                    title: const Text('Total Time Played:'),
-                    subtitle: Center(
-                        child: Text(msToTimeString(
-                            snapshot.data![0][0]['total_ms_played']))),
-                  ),
-                ),
-                Card(
-                    child: ListTile(
-                  title: const Text('Most Streamed Day:'),
-                  subtitle: Text(
-                      'The most streamed day was ${snapshot.data![1][0]['day']}.\nYou played ${msToTimeString(snapshot.data![1][0]['total_ms_played'])} on this day.'),
-                )),
-                Card(
-                    child: ListTile(
-                  title: const Text('Your Average Listening Day:'),
-                  subtitle: Text(
-                      'You listen to music for ${msToTimeString(snapshot.data![2][0]['average_ms_played'])} on your average day.'),
-                )),
+                TotalPlayTime(
+                    msPlayed: snapshot.data![0][0]['total_ms_played']),
+                MostStreamedDay(
+                    date: snapshot.data![1][0]['day'],
+                    msPlayed: snapshot.data![1][0]['total_ms_played']),
+                AverageListeningDay(
+                    averageMsPlayed: snapshot.data![2][0]['average_ms_played']),
+                FavouriteTimeOfDay(hoursOfDay: snapshot.data![3]),
               ],
             );
           });
